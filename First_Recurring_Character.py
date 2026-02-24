4048e6b47cbe7e600d292d86b8b75661bc1d5893
Vanhan 25091397071 B <wardanarheda@gmail.com>
2026-02-24 21:18:03 +0700

    Add files via upload

---
+++ First_Recurring_Character.py
@@ -0,0 +1,9 @@
+def first_recurring(text):
+    sudah_ada = set()
+
+    for huruf in text:
+        if huruf in sudah_ada:
+            return huruf
+        sudah_ada.add(huruf)
+
+    return None
